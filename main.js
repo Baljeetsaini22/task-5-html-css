9094ae35724e103c28a70c8b095d8ba8aa6e29c7
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-04-14 09:33:40 +0530

    first

---
+++ main.js
@@ -0,0 +1,152 @@
+// const student = {
+//     name: "Baljeet",
+//     age: 25,
+//     cgpa: 8.2,
+//     ispass: true
+// }
+
+// //console.log(student.fullname);
+
+// student['name'] = "Vikas"
+// console.log(student.name)
+
+// const proudct = {
+//     title: "Ball Pen",
+//     rating: 4,
+//     offer: 5, 
+//     price:270,
+// }
+// console.log(proudct.title)
+
+// arithmetic operators
+// let a = 5 ;
+// let b = 2 ;
+
+// console.log("a =", a, "& b =", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+// let a = 5 ;
+// let b = 2 ;
+
+// console.log("a =", a,);
+// console.log("++a =", ++a);
+// //console.log("a =", a);
+
+// asignment operator
+// let a = 5 ;
+// let b = 2 ;
+
+// a /= 4; //a = a +4
+// console.log("a =", a);
+
+
+// comperison operator
+// let a = 8;
+// let b = "10";
+// console.log("5 == 2", a == b);
+// console.log("5 != 2", a != b);
+// console.log("5 === 2", a === b);
+// console.log("5 > 2", a > b);
+// console.log("5 < 2", a < b);
+// console.log("5 <= 2", a <= b);
+// console.log("5 >= 2", a >= b);
+
+
+// logical operator 
+// let a = 6;
+// let b = "4";
+
+// let cond1 = a < b;
+// let cond2 = a === 6;
+
+// console.log("cond1 && cond2", a < b && a === 6);
+
+
+// console.log("cond1 || cond2 =", a < b || a===6);
+// console.log("!(6 < 5) =", !(a === b));
+
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+
+// if (age < 18) {
+//     console.log("you CANNOT vote");
+// }
+
+
+
+// condition statements if statement 
+// let mode = "light"
+// let color;
+// if(mode === "dark") {
+//     color = "black";
+// }
+// if (mode === "light") {
+//     color = "white";
+// }
+// console.log(color);
+
+// else statement
+// let mode = "light"
+// let color;
+// if(mode === "dark") {
+//     color = "black";
+// }
+// else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let num = 21;
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// }
+// else {
+//     console.log(num, "is odd");
+// }
+
+//else-if statement 
+
+// let age = 75;
+// if(age < 18) {
+//     console.log("junior");
+// }
+// else if(age > 60 ) {
+//     console.log("senior");
+// }
+// else {
+//     console.log("middle");
+// }
+
+// let mode = "vikas"
+// let color;
+
+// if(mode === "dark") {
+//     color = "black";
+// }
+// else if ( mode === "blue") {
+//     color = "blue";
+// }
+// else if (mode === "pink") {
+//     color = "pink";
+// }
+// else {
+//     color = "white"
+// }
+// console.log(color);
+
+
+//operators in java script 
+// + operator
+
+
+
+
